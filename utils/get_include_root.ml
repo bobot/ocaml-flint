31929483d9a615aacab1919f67b3c64e05147f6e
François Bobot <francois.bobot@cea.fr>
2022-03-16 00:01:44 +0100

    Fix include path
        and hack against dune problems

--- utils/get_include_root.ml
+++ utils/get_include_root.ml
@@ -1,4 +1,6 @@
 let () =
-  print_endline
-    (Filename.concat (Sys.getcwd ())
-       (Filename.dirname (Filename.dirname (Filename.dirname Sys.argv.(1)))))
+  let r = ref Sys.argv.(2) in
+  for i = 1 to int_of_string Sys.argv.(1) do
+    r := Filename.dirname !r
+  done;
+  print_string (Filename.concat (Sys.getcwd ()) !r)
